acdbadd32884d0326ea5fbcdf12a9a66b9548136
igor <igor.s10@aluno.ifsc.edu.br>
2025-04-10 19:51:14 -0300

    Desenvolvidos métodos para imprimir as strings em ambos formatos sugeridos no enunciado E melhorado algoritmo para tratar entrada invalida.

--- app/src/main/java/ifsc/poo/Relogio.java
+++ app/src/main/java/ifsc/poo/Relogio.java
@@ -6,16 +6,51 @@ public class Relogio {
 
     void ajustaHora(byte hora, byte minuto, byte segundo){
 
-        this.segundo = (byte) Math.abs(segundo);
-        this.minuto = (byte) Math.abs(minuto);
-        this.hora = (byte) Math.abs(hora);
+        // Se hora for maior que 23, será 0
+        // Se minuto ou segundo for maior que 60, será 0
+
+        if(segundo < 60)
+            this.segundo = (byte) Math.abs(segundo);
+        else this.segundo = 0; 
+        
+        if(minuto < 60)    
+            this.minuto = (byte) Math.abs(minuto);
+        else this.minuto = 0;
+        
+        if(hora < 24)
+            this.hora = (byte) Math.abs(hora);
+        else this.hora = 0;
 
     }
 
     String getHora(){
 
-        return this.hora + ":" + this.minuto + ":" + this.segundo;
+        // Aqui eu faço uma verificação se todos os parametros são maiores que 9
+        // pois não precisa concatenar com "0"
+        if(this.hora > 9 && this.minuto > 9 && this.segundo > 9)
+            return this.hora + ":" + this.minuto + ":" + this.segundo;
 
+        // Se hora, minuto ou segundo for menor que 9, eu concateno "0" na string
+        String horario = this.hora < 10 ? "0" + this.hora + ":": this.hora + ":";
+
+        horario += this.minuto < 10 ? "0" + this.minuto + ":" : this.minuto + ":" ;
+
+        horario += this.segundo < 10 ? "0" + this.segundo : this.segundo + "";
+
+        return horario;
+    }
+
+    String getHoraFormatoV2(){
+
+        // Se hora, minuto ou segundo for menor que 9, eu concateno "0" na string
+
+        String horario = this.hora < 10 ? "0" + this.hora + "am " : this.hora + "pm ";
+
+        horario += this.minuto < 10 ? "0" + this.minuto + "m ": this.minuto + "m ";
+
+        horario += this.segundo < 10 ? "0" + this.segundo + "s ": this.segundo + "s ";
+
+        return horario;
     }
 
     void avancaHora(){
