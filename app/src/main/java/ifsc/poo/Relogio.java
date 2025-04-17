da9e1babb8bb53be2a5024bfd226ad664c08fda3
Igor <igor.s10@aluno.ifsc.edu.br>
2025-04-17 18:07:56 -0300

    Implementado métodos propostos pela respectiva questão do Laboratório 2. Testei e funcionou.

--- app/src/main/java/ifsc/poo/Relogio.java
+++ app/src/main/java/ifsc/poo/Relogio.java
@@ -1,7 +1,5 @@
 package ifsc.poo;
 
-import java.awt.RenderingHints;
-
 /*
  * Os métodos de setar hora, minuto e segundo
  * podem receber qualquer valor float, pois será
@@ -12,36 +10,62 @@ public class Relogio {
 
     private byte hora, minuto, segundo;
 
-    public void ajustaHora(byte hora, byte minuto, byte segundo){
+    public Relogio(byte hora){
+
+        this(hora, (byte) 0, (byte) 0);
+
+    }    
+
+    public Relogio(byte hora, byte minuto){
+
+        this(hora, minuto, (byte) 0);
+
+    }   
+
+    public Relogio(byte hora, byte minuto, byte segundo){
+
+        this.ajustaHora(hora, minuto, segundo);
+
+    }
+
+    private void ajustaHora(byte hora, byte minuto, byte segundo){
 
         // Se hora for maior que 23, será 0
         // Se minuto ou segundo for maior ou igual que 60, será 0
 
-        if(segundo < 60)
-            this.segundo = (byte) Math.abs(segundo);
-        else this.segundo = 0; 
+        if(segundo >= 60 || segundo < 0)
+            this.segundo = 0; 
+        else this.segundo = segundo;
         
-        if(minuto < 60)    
-            this.minuto = (byte) Math.abs(minuto);
-        else this.minuto = 0;
+        if(minuto >= 60 || minuto < 0)
+            this.minuto = 0;
+        else this.minuto = minuto;
         
-        if(hora < 24)
-            this.hora = (byte) Math.abs(hora);
-        else this.hora = 0;
+        if(hora >= 24 || hora < 0)
+            this.hora = 0;
+        else this.hora = hora;
 
     }
 
-    public String getHora(){
+    public byte getHora(){
+        return this.hora;
+    }
+
+    public byte getMinuto(){
+        return this.minuto;
+    }
+
+    public byte getSegundo(){
+        return this.segundo;
+    }
+
+    public String getHoraFormatoV1(){
 
         return String.format("%02d:%02d:%02d", this.hora, this.minuto, this.segundo);
 
     }
 
     public String getHoraFormatoV2(){
-
-        //String horario = this.hora < 10 ? "0" + this.hora : ((this.hora - 12) < 10 ? "0" + Math.abs(this.hora - 12) : this.hora + "");
-        // Apliquei operador ternário dentro de outro operador ternário
-        //String horario = this.hora < 10 ? "0" + this.hora : (this.hora > 12 ? Math.abs(this.hora - 12) + "": "0" + this.hora);
 
         String horario = "";
         if(this.hora <= 12){
@@ -89,7 +113,6 @@ public class Relogio {
 
     }
 
-
     public void avancaSegundo(){
 
         this.segundo++;
@@ -100,4 +123,33 @@ public class Relogio {
         }
 
     }
+
+    // ========= MÉTODOS PARA A DIFERENÇA ENTRE DOIS HORARIOS ==========
+    // Converte um um relogio (hh:mm:ss) para segundo
+    private int horarioToSegundo(Relogio r){
+
+        return r.hora * 3600 + r.minuto * 60 + r.segundo;
+
+    }
+
+    // Retorna o módulo da diferença entre dois horarios
+    public int diffRelogios(Relogio r1, Relogio r2){
+
+        return Math.abs(horarioToSegundo(r1) - horarioToSegundo(r2));
+
+    }
+
+    // ========= MÉTODO PARA SINCRONIZAR DOIS RELOGIOS ========
+    // O Relogio R1 será sincronizado com o horário do Relógio R2
+    public static boolean sincronizarRelogios(Relogio r1, Relogio r2){
+
+        if(r1 == null || r2 == null)
+            return false;
+
+        r1.ajustaHora(r2.getHora(), r2.getMinuto(), r2.getSegundo());
+
+        return true;
+
+    }
+
 }
