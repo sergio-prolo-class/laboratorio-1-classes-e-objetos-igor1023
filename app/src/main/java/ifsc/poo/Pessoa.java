4b179b2cc73b2c096ee1cc1b8ea199fcbc08c465
Aluno <igor.s10@aluno.ifsc.edu.br>
2025-04-17 09:02:05 -0300

    Realizadas alterações na classe Pessoa de acordo com a proposta da respectiva questão do Laboratório 2, no entanto, tenho que verificar a questão do CPF ser constante (final)...

--- app/src/main/java/ifsc/poo/Pessoa.java
+++ app/src/main/java/ifsc/poo/Pessoa.java
@@ -9,14 +9,72 @@ public class Pessoa {
 
     private String nome;
     private int idade;
+    private String cpf;
 
-    public void setNome(String nome){
-        if(nome == null || nome.isEmpty() || nome.isBlank())
-            this.nome = null;
-        else this.nome = ajustaNome(nome);
+    public Pessoa(){
+        setCpf("123.456.789-00");
+        setNome("");
+        setIdade(0);
+    }
+
+    public Pessoa(String cpf){
+
+        setCpf(cpf);
+        this.nome = "";
+        this.idade = 0;
+    }
+
+    public Pessoa(String cpf, String nome){
+
+        setCpf(cpf);
+        setNome(nome);
+        this.idade = 0;
+    }   
+
+    public Pessoa(String cpf, String nome, int idade){
+
+        setCpf(cpf);
+        setNome(nome);
+        setIdade(idade);
+
+    }
+
+    private String formatoCpfCorreto(String str){
+
+        if(str == null)
+            return null;
+
+        if(! (str.isEmpty() && str.isBlank()))
+            return str;
+        
+        return "123.456.789-00";        
+
+    }
+
+    private boolean setCpf(String cpf){
+
+        this.cpf = formatoCpfCorreto(cpf);
+        return this.cpf != null;
+
+    }
+
+    public String getCpf(){
+
+        return this.cpf;
+
+    }
+
+    public boolean setNome(String nome){
+
+        this.nome = ajustaNome(nome);
+        return this.nome != null;
     }
 
     private String ajustaNome(String str){
+
+        if(str == null || str.isEmpty() || str.isBlank()) {
+            return null;
+        }
 
         str = str.trim();
         str = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
@@ -28,8 +86,15 @@ public class Pessoa {
         return this.nome;
     }
 
-    public void setIdade(int idade){
-        this.idade = Math.abs(idade); 
+    public boolean setIdade(int idade){
+        
+        if(idade < 0){
+            this.idade = 0;
+            return false;
+        }
+        
+        this.idade = idade;
+        return true; 
     }
 
     public int getIdade(){
