fdec605de7bf9eed30142625e5a1f7fc16f803de
Aluno <igor.s10@aluno.ifsc.edu.br>
2025-04-17 09:18:47 -0300

    Iniciada edição/implementação da classe Retangulo seguindo propostas da respectiva questão do Laboratorio 2.

--- app/src/main/java/ifsc/poo/Retangulo.java
+++ app/src/main/java/ifsc/poo/Retangulo.java
@@ -1,5 +1,7 @@
 package ifsc.poo;
 
+import com.google.j2objc.annotations.RetainedWith;
+
 /*
  * Os métodos de setar largura e altura
  * podem receber qualquer valor float, pois será
@@ -7,8 +9,24 @@ package ifsc.poo;
 */
 
 public class Retangulo {
+    
+    // Constantes
+    final private int LARGURA_PADRAO = 1;
+    final private int ALTURA_PADRAO = 1;
 
+    // Atributos
     private float largura, altura;
+
+    static Retangulo maiorArea;
+    static Retangulo menorPerimetro;
+
+    public Retangulo(float l, float a){
+
+        this.largura = l <= 0 ? LARGURA_PADRAO : l;
+        this.altura  = l <= 0 ? ALTURA_PADRAO  : a;
+
+
+    }
 
     public void setLargura(float largura){
         this.largura = Math.abs(largura);
@@ -26,4 +44,17 @@ public class Retangulo {
         return (float) (2.0 * (this.largura + this.altura));
     }
 
+    private void atualizaDados(){
+
+
+
+    }
+
+    public static Retangulo getMaiorArea(){
+        return maiorArea;
+    }
+
+    public static Retangulo getmenorPerimetro(){
+        return menorPerimetro;
+    }
 }
