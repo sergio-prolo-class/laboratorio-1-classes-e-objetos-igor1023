d013ff722522d8f79d26d3db45f737da30b9937c
Aluno <igor.s10@aluno.ifsc.edu.br>
2025-04-17 08:02:04 -0300

    Editada classe Lampada.java de acordo com a proposta da respectiva questão do Laboratório 2. Realizei testes e funcionou.

--- app/src/main/java/ifsc/poo/Lampada.java
+++ app/src/main/java/ifsc/poo/Lampada.java
@@ -3,17 +3,32 @@ package ifsc.poo;
 public class Lampada {
 
     private boolean ligada;
+    private int quantidadeLampadas = 0;
 
-    public void ligar (){
-        this.ligada = true;
+    public Lampada(){
+        this.ligada = false; // Já é padrão...
+        this.quantidadeLampadas++;
     }
 
-    public void desligar (){
-        this.ligada = false;
+    public Lampada(boolean estado){
+        this.ligada = estado;
+        this.quantidadeLampadas++;
+    }
+
+    public boolean ligar (){
+        return this.ligada = true;
+    }
+
+    public boolean desligar (){
+        return this.ligada = false;
     }
 
     public boolean verEstado(){
         return this.ligada;
+    }
+
+    public int quantidadeLampadas(){
+        return this.quantidadeLampadas;
     }
 
 }
